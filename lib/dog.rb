c8cfbb9d242477d968a8a2b9d50a268cd4b24613
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-24 14:45:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,9 +14,9 @@ class Dog
 	  dog
 	 end 
 	 
-# 	 def self.print_all
-# 	  self.all.each do {|dog| puts "#{dog.name}"}
-# 	 end 
+	 def self.print_all
+	  self.all.each {|dog| puts "#{dog.name}"}
+	 end 
 	 
 
 	def self.clear_all
